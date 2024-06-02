1d15e597da79ee700e2fdf9f193062daf35ecf8b
pankarvishal <113195014+pankarvishal@users.noreply.github.com>
2024-06-02 18:04:25 +0530

    Add files via upload

---
+++ videeo39.java
@@ -0,0 +1,25 @@
+
+class Employee{
+    int salary;
+    String name;
+
+public int getsalary(){
+    return salary;
+}
+
+public String getname(){
+    System.out.println(name);
+
+}
+
+public void
+
+}
+
+
+public class videeo39 {
+public static void main(String[] args) {
+    
+}
+    
+}
